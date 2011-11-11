4b91ea8c4c5fee618565f19a646d5a3ffeea3751
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-11-11 22:00:47 +0200

    Added automaton package using IKVM for comparing the results between Java and .NET.

---
+++ Src/NAutomaton.Tests.Integration/Java/XegerTests.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * Copyright 2009 Wilfred Springer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Text.RegularExpressions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NAutomaton.Tests.Integration.Java
+{
+    public sealed class XegerTests
+    {
+        [Theory]
+        [InlineData("[ab]{4,6}c")]
+        [InlineData("(a|b)*ab")]
+        public void ShouldGenerateTextCorrectly(string regex)
+        {
+            var generator = new Java.Xeger(regex);
+            for (int i = 0; i < 100; i++)
+            {
+                string text = generator.Generate();
+                Assert.True(Regex.IsMatch(text, regex));
+            }
+        }
+    }
+}
