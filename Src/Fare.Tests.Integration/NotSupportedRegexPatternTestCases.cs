8ecb2d9af4773aef26fb4c5795d2d4b7ac83b1ca
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-12-15 12:42:31 +0200

    Add        Src/Fare.Tests.Integration/NotSupportedRegexPatternTestCases.cs Modified   Src/Fare.Tests.Integration/RegexPatternTestCases.cs Modified   Src/Fare.Tests.Integration/XegerTests.cs Modified   Src/Fare.Tests.Integration/Fare.Tests.Integration.csproj

---
+++ Src/Fare.Tests.Integration/NotSupportedRegexPatternTestCases.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Fare.Tests.Integration
+{
+    public class NotSupportedRegexPatternTestCases : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { @"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$" };
+            yield return new object[] { "[" };
+            yield return new object[] { @"(?\[Test\]|\[Foo\]|\[Bar\])?(?:-)?(?\[[()a-zA-Z0-9_\s]+\])?(?:-)?(?\[[a-zA-Z0-9_\s]+\])?(?:-)?(?\[[a-zA-Z0-9_\s]+\])?(?:-)?(?\[[a-zA-Z0-9_\s]+\])?" };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
