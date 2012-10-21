de28291391f1dcdc39194505293d5907da5f5d54
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-10-21 17:51:21 +0300

    Removed test cases that were executing through IKVM.NET. The results are now compared with the Rex engine from Microsoft Research.

--- Src/Fare.Tests.Integration/XegerTests.cs
+++ Src/Fare.Tests.Integration/XegerTests.cs
@@ -6,17 +6,21 @@ using Xunit.Extensions;
 
 namespace Fare.Tests.Integration
 {
-    /// <summary>
-    /// Regex pattern test cases for Xeger (.NET port through Fare package).
-    /// </summary>
     public sealed class XegerTests
     {
-        [Theory]
-        [ClassData(typeof(RegexPatternTestCases))]
+        [Theory, ClassData(typeof(RegexPatternTestCases))]
         public void GeneratedTextIsCorrect(string pattern)
         {
             var sut = new Fare.Xeger(pattern);
             var result = Enumerable.Range(1, 3).Select(i => sut.Generate()).ToArray();
+            Array.ForEach(result, regex => Assert.True(Regex.IsMatch(regex, pattern)));
+        }
+
+        [Theory, ClassData(typeof(RegexPatternTestCases))]
+        public void GeneratedTextIsCorrectWithRexEngine(string pattern)
+        {
+            var settings = new Rex.RexSettings(pattern) { k = 1 };
+            var result = Enumerable.Range(1, 3).Select(i => Rex.RexEngine.GenerateMembers(settings).Single()).ToArray();
             Array.ForEach(result, regex => Assert.True(Regex.IsMatch(regex, pattern)));
         }
     }
