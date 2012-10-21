8334943b061110bd1daee833f169b8e477265d81
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-10-21 20:02:02 +0300

    Removed Automata directory.

--- Src/Fare.Tests.Integration/Automata/AutomataZ3Samples/buildSamples.bat
+++
@@ -1,4 +0,0 @@
-copy ..\bin\Microsoft.Automata.dll .
-copy ..\bin\Microsoft.Automata.Z3.dll .
-copy ..\bin\Microsoft.Z3.dll .
-csc /target:exe /platform:x86 /out:run.exe Program.cs /r:Microsoft.Automata.dll /r:Microsoft.Automata.Z3.dll /r:Microsoft.Z3.dll
