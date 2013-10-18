eba5005d40fa8b267ec36a8430a076ac4e6a35d2
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-10-18 17:29:47 +0300

    Enabled auto-incrementing assembly version(s).

--- Src/Fare/Properties/AssemblyInfo.cs
+++ Src/Fare/Properties/AssemblyInfo.cs
@@ -20,6 +20,7 @@ using System.Runtime.InteropServices;
 //// COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 //// The following GUID is for the ID of the typelib if this project is exposed to COM
 
@@ -34,6 +35,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-
-[assembly: CLSCompliant(true)]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
