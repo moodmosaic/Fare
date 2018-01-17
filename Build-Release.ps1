1a469433bf2e32f0b1fd2676b724507a443a8f0e
Alex Povar <zvirja1@gmail.com>
2018-01-17 12:13:58 +0200

    Add FAKE build script to make the project

--- Build-Release.ps1
+++
@@ -1,9 +0,0 @@
-function Get-VsVersion ()
-{
-    if ($env:VS120COMNTOOLS -ne $null)
-    {
-        " /property:VisualStudioVersion=12.0"
-    }
-}
-
-Start-Process -NoNewWindow -Wait -FilePath $env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild -ArgumentList ('BuildRelease.msbuild', '/property:Configuration=Release', (Get-VsVersion))
